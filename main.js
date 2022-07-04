b24de958ec229965bfa64d88791aa89b6da2a894
ErJump <fedelijunior@gmail.com>
2022-07-04 13:44:37 +0200

    Adds an image by data

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const app = new Vue ({
     el: '#root',
     data: {
         message: 'Hello Vue!',
+        imgSource : 'https://thumbs.gfycat.com/PinkPiercingBull-size_restricted.gif',
     },
 });
